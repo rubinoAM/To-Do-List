acfb69c6b9c8adc8d0eb950a3a8d7935444a2847
rubinoAM <parkcathedralgrace@gmail.com>
2019-02-19 15:31:41 -0500

    Route for addTask | CORS Error Prevention | Config in place

--- back-end/app.js
+++ back-end/app.js
@@ -9,6 +9,13 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+//Prevent CORS Errors
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
--- back-end/routes/index.js
+++ back-end/routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+const mysql = require('mysql');
+const config = require('../config');
+const connection = mysql.createConnection(config);
+connection.connect();
+
+// GET home page
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+// Add Task
+router.post('/addTask', (req,res,next) => {
+  const taskName = req.body.taskName;
+  const taskDate = req.body.taskDate;
+
+  res.json({taskName,taskDate});
 });
 
 module.exports = router;
