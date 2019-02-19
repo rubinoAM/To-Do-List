5e9b7d3626b8dde7a28887e4b76c450b04cfbd82
rubinoAM <parkcathedralgrace@gmail.com>
2019-02-19 16:03:20 -0500

    Connects to dB

--- back-end/routes/index.js
+++ back-end/routes/index.js
@@ -15,8 +15,17 @@ router.get('/', function(req, res, next) {
 router.post('/addTask', (req,res,next) => {
   const taskName = req.body.taskName;
   const taskDate = req.body.taskDate;
+  const insertQuery = `INSERT INTO tasks(taskName,taskDate)
+    VALUES (?,?);`;
 
-  res.json({taskName,taskDate});
+  connection.query(insertQuery,[taskName,taskDate],(err,results)=>{
+    if(err){throw err;}
+    const getTasksQuery = `SELECT * FROM tasks;`;
+    connection.query(getTasksQuery,(nextErr,nextRes)=>{
+      if(nextErr){throw nextErr;}
+      res.json({nextRes})
+    });
+  })
 });
 
 module.exports = router;
